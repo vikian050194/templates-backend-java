7f7311c1b448ba5f6948e6a6388911e021bc7f72
Kirill Vinogradov <vikian050194@gmail.com>
2022-02-09 23:22:33 +0100

    Add Simple project

---
+++ Simple/src/app/Simple.java
@@ -0,0 +1,12 @@
+package app;
+
+class Simple {
+    public static void main(String args[]) {
+        System.out.println("Hello Java");
+
+        // Using Console to input data from user
+        String name = System.console().readLine();
+
+        System.out.println("You entered string " + name);
+    }
+}
---
+++ Simple/test/FooTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class FooTest {
+
+    @Test
+    public void TestBar() {
+        assertEquals(4 * 10 + 2, 33);
+    }
+}
